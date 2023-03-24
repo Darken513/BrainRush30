2d3fc2e3bec2d83d05fa765d0ee9df6ccf12ffc4
Darken <learningstuffs513@gmail.com>
2023-03-24 01:17:10 +0100

    new style

--- BrainRushGUI/src/app/home-page/home-page.component.ts
+++ BrainRushGUI/src/app/home-page/home-page.component.ts
@@ -7,21 +7,43 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-  
-  difficulty:Array<any> = new Array(10);
-  daysProgress:Array<any> = new Array(30);
-  currentDay:number = 15;
+
+  difficulty: Array<any> = new Array(10);
+  daysProgress: Array<any> = new Array(30).fill(1);
+  currentDay: number = 15;
   currUser: any;
 
   constructor(
-    private authService:AuthService
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    this.daysProgress = this.daysProgress.map((val, idx)=>idx);
+    this.daysProgress = this.daysProgress.map((val, idx) => {
+      return {
+        difficulty: new Array(Math.floor(idx / 3) < 1 ? 1 : Math.floor(idx / 3)).fill(1),
+        grade: this.getGrade()
+      }
+    });
     this.currUser = this.authService.getCurrentUser();
   }
+  getGrade() {
+    let rand = Math.floor(Math.random() * 3)
+    switch (rand) {
+      case 0:
+        return "acceptable"
+        break;
+      case 1:
+        return "good"
+        break;
+      case 2:
+        return "Excelent"
+        break;
 
+      default:
+        return 'acceptable'
+        break;
+    }
+  }
   logout(): void {
     this.authService.logout();
   }
